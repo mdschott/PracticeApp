411c76bf4ccf1b1f4a9c584c90351810442f7868
mschott354 <mschott@mail.bradley.edu>
2018-01-24 23:23:53 -0600

    Initial commit

---
+++ app/src/main/java/com/example/michael/practiceapp/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.michael.practiceapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "AndroidMessage";
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i(TAG, "onStart");
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.i(TAG, "onCreate");
+    }
+}
