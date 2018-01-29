da3980490cd0202cfbbd1a34db41336a63a84b28
mdschott <mschott@mail.bradley.edu>
2018-01-28 21:13:18 -0600

    Created widgets and set their properties.

--- app/src/main/java/com/example/michael/practiceapp/MainActivity.java
+++ app/src/main/java/com/example/michael/practiceapp/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.michael.practiceapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.RelativeLayout;
+import android.widget.Button;
+import android.graphics.Color;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,7 +21,43 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        RelativeLayout.LayoutParams buttonDetails = new RelativeLayout.LayoutParams(
+                RelativeLayout.LayoutParams.WRAP_CONTENT,
+                RelativeLayout.LayoutParams.WRAP_CONTENT
+        );
+
+        buttonDetails.addRule(RelativeLayout.CENTER_HORIZONTAL);
+        buttonDetails.addRule(RelativeLayout.CENTER_VERTICAL);
+
+        Button redButton = new Button(this);
+        redButton.setText("Log In");
+        redButton.setBackgroundColor(Color.RED);
+        redButton.setId(1);
+
+        RelativeLayout myLayout = new RelativeLayout(this);
+
+
+        //Username input
+        EditText username = new EditText(this);
+        username.setId(2);
+
+        RelativeLayout.LayoutParams usernameDetails = new RelativeLayout.LayoutParams(
+                RelativeLayout.LayoutParams.WRAP_CONTENT,
+                RelativeLayout.LayoutParams.WRAP_CONTENT
+        );
+
+        usernameDetails.addRule(RelativeLayout.ABOVE, redButton.getId());
+        usernameDetails.addRule(RelativeLayout.CENTER_HORIZONTAL);
+        usernameDetails.setMargins(0,0,0,50);
+
+
+
+
+        myLayout.addView(redButton, buttonDetails);
+        myLayout.addView(username, usernameDetails);
+
+        setContentView(myLayout);
         Log.i(TAG, "onCreate");
     }
 }
